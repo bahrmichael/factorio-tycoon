f33342fda159d8aa2f2d2bc96ff994166998e9cd
bahrmichael <michael@bahr.dev>
2023-11-04 15:19:17 +0100

    chore: reduce city radius

--- constants.lua
+++ constants.lua
@@ -4,9 +4,9 @@ local CONSTANTS = {
     -- Each cell has 6x6 tiles
     CELL_SIZE = 6,
     CITY_GROWTH_TICKS = OneSecond * 30,
-    CITY_RADIUS = 250,
+    CITY_RADIUS = 150,
     -- Minimum ticks until we try adding another city
-    MORE_CITIES_TICKS = OneSecond * 60, 
+    MORE_CITIES_TICKS = OneSecond * 60,
 }
 
 return CONSTANTS
