bef986e3abe2eab367c6d370a314d6bfea2c71ab
bahrmichael <michael@bahr.dev>
2023-11-04 16:25:53 +0100

    feat: add tips and tricks for funding new cities

--- prototypes/gui/tips-and-tricks.lua
+++ prototypes/gui/tips-and-tricks.lua
@@ -88,4 +88,15 @@ data:extend({
             technology = "tycoon-highrise-housing"
         },
     },
+    {
+        type = "tips-and-tricks-item",
+        name = "tycoon-fund-new-cities",
+        category = "tycoon",
+        order = "a[tycoon]-f",
+        indent = 1,
+        trigger = {
+            type = "research",
+            technology = "tycoon-multiple-cities"
+        },
+    },
   })
