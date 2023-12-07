2a3255687bbca355d7702cec925fa007b64bcaee
bahrmichael <michael@bahr.dev>
2023-12-07 17:28:55 +0100

    fix: use different ticks for city expansion to avoid overlaps

--- constants.lua
+++ constants.lua
@@ -6,7 +6,7 @@ local CONSTANTS = {
     CITY_GROWTH_TICKS = OneSecond * 60,
     CITY_RADIUS = 150,
     -- Minimum ticks until we try adding another city
-    MORE_CITIES_TICKS = OneSecond * 60,
+    MORE_CITIES_TICKS = OneSecond * 120,
     INITIAL_CITY_TICK = 30,
     PASSENGER_SPAWNING_TICKS = OneSecond * 2,
 }
