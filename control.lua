d694674ec9ea3a84e099a875014429fe574e0b74
Michael Bahr <michael@bahr.dev>
2023-12-24 18:21:36 +0100

    fix: prevent a rare crash when a city disappeared

--- control.lua
+++ control.lua
@@ -485,23 +485,27 @@ script.on_event(defines.events.on_player_cursor_stack_changed, function(event)
 
             for _, city in ipairs(global.tycoon_cities or {}) do
 
-                local r = rendering.draw_circle{
-                    color = {0.1, 0.2, 0.1, 0.01},
-                    -- todo: add tech that increases this range, but only up to 250 which is the max for building cities all over the map
-                    radius = Constants.CITY_RADIUS,
-                    filled = true,
-                    target = city.special_buildings.town_hall,
-                    surface = game.surfaces[1],
-                    draw_on_ground = true,
-                }
+                -- Added this condition, because there was a case where a city was gone in a multiplayer (no idea why yet),
+                -- and then the game would crash upon rendering the circle.
+                if (city.special_buildings or {}).town_hall ~= nil and (city.special_buildings or {}).town_hall.valid then
+                    local r = rendering.draw_circle{
+                        color = {0.1, 0.2, 0.1, 0.01},
+                        -- todo: add tech that increases this range, but only up to 250 which is the max for building cities all over the map
+                        radius = Constants.CITY_RADIUS,
+                        filled = true,
+                        target = city.special_buildings.town_hall,
+                        surface = game.surfaces[1],
+                        draw_on_ground = true,
+                    }
 
-                if global.tycoon_player_renderings == nil then
-                    global.tycoon_player_renderings = {}
+                    if global.tycoon_player_renderings == nil then
+                        global.tycoon_player_renderings = {}
+                    end
+                    if global.tycoon_player_renderings[event.player_index] == nil then
+                        global.tycoon_player_renderings[event.player_index] = {}
+                    end
+                    table.insert(global.tycoon_player_renderings, r)
                 end
-                if global.tycoon_player_renderings[event.player_index] == nil then
-                    global.tycoon_player_renderings[event.player_index] = {}
-                end
-                table.insert(global.tycoon_player_renderings, r)
             end
         else
             rendering.clear("tycoon")
@@ -645,7 +649,7 @@ script.on_event(defines.events.on_gui_opened, function (gui)
         end
 
         local anchor = {gui = defines.relative_gui_type.container_gui, name = gui.entity.name, position = defines.relative_gui_position.right}
-        supplyBuildingView = player.gui.relative.add{type = "frame", anchor = anchor, caption = {"", {"entity-name." .. gui.entity.name}}, direction = "vertical", name = guiKey}
+                supplyBuildingView = player.gui.relative.add{type = "frame", anchor = anchor, caption = {"", {"entity-name." .. gui.entity.name}}, direction = "vertical", name = guiKey}
 
         Gui.addSupplyBuildingOverview(supplyBuildingView, cityName)
     end
