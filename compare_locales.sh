722563b4ac7f435f49958a9aafe274459a75a793
Roman Vasiyarov <rvasiyarov@gmail.com>
2024-03-07 18:15:46 +0300

    chore: compare-locales.sh: simplify, add error counting and reporting

--- compare_locales.sh
+++ compare_locales.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function error() {
+    echo "ERROR:" "$@" >&2
+}
+
 # Extracts groups, keys and/or any other necessary information
 function extract_keys() {
     grep --no-messages -E -o '^(\[[^\s]*\]$|[^=]+=)' "$1"
@@ -20,6 +24,7 @@ while IFS= read -r line; do
 done < <(find "$base_dir" -mindepth 1 -maxdepth 1 -type d -not -name "$base_lang" -printf "%P\n" | LC_ALL=C sort)
 
 # Iterate over all files in the base language directory
+let ERRORS=0
 for source_file in "$base_dir/$base_lang"/*.cfg; do
     filename=$(basename "$source_file")
 
@@ -29,22 +34,22 @@ for source_file in "$base_dir/$base_lang"/*.cfg; do
         target_file="$base_dir/$lang/$filename"
 
         # Check if the target file exists
-        if [[ -f "$target_file" ]]; then
-            echo "Comparing $source_file with $target_file"
-            # Use diff to compare files, you can also use cmp or other tools
-            # diff "$file" "$target_file" > /dev/null
-            diff -q <(extract_keys "$source_file") <(extract_keys "$target_file")
-            if [[ $? -eq 0 ]]; then
-                # echo "No differences found."
-                echo ""
-            else
-                echo "Differences found!"
-                diff <(extract_keys "$source_file") <(extract_keys "$target_file")
-                exit 1
-            fi
-        else
-            echo "File $lang/$filename does not exist."
-            exit 1
-        fi
+        [[ ! -f $target_file ]] && {
+            error "File does not exist: $target_file"
+            let ERRORS+=1
+            continue
+        }
+
+        echo "Comparing $source_file with $target_file"
+        diff -U0 -N --label "$source_file" <(extract_keys "$source_file") --label "$target_file" <(extract_keys "$target_file") || {
+            let ERRORS+=1
+        }
     done
 done
+
+# Notify of any errors
+[[ $ERRORS -gt 0 ]] && error "Total errors: $ERRORS"
+
+
+# Finally, cleanup and exit
+exit $ERRORS
