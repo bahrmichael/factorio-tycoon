a35ecf566644dffb92585e9509fdaf32ca797974
Roman Vasiyarov <rvasiyarov@gmail.com>
2024-04-01 17:41:24 +0300

    chore: grid-util add few functions

--- grid-util.lua
+++ grid-util.lua
@@ -26,6 +26,19 @@ local function safeGridAccess(city, coordinates, sendWarningForMethod)
 end
 
 --- @param city City
+--- @param coordinates Coordinates
+--- @param value Cell
+--- @return any | nil cell
+local function safeGridSet(city, coordinates, value)
+    local row = city.grid[coordinates.y]
+    if row ~= nil then
+        -- make sure we don't set local
+        city.grid[coordinates.y][coordinates.x] = value
+        return row[coordinates.x]
+    end
+end
+
+--- @param city City
 local function getOffsetX(city)
     -- -3 is because the center starts at the top left of the initial 3x3 grid. so at the beginning there must be no additional offset.
     -- but then it should grow by one for each grid expansion (each expansion is 2 rows and columns, so we divide it by 2)
@@ -55,10 +68,18 @@ local function translateCityGridToTileCoordinates(city, coordinates)
     }
 end
 
+local function translateToGrid(city, position)
+    local x = 1 + math.floor(math.floor(position.x - getOffsetX(city)) / Constants.CELL_SIZE)
+    local y = 1 + math.floor(math.floor(position.y - getOffsetY(city)) / Constants.CELL_SIZE)
+    return {x = x, y = y}
+end
+
 return {
     getGridSize = getGridSize,
     getOffsetX = getOffsetX,
     getOffsetY = getOffsetY,
     safeGridAccess = safeGridAccess,
+    safeGridSet = safeGridSet,
     translateCityGridToTileCoordinates = translateCityGridToTileCoordinates,
+    translateToGrid = translateToGrid,
 }
