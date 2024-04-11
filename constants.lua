1a1b584a801cb10ec828265b6a1de77fde949cff
Roman Vasiyarov <rvasiyarov@gmail.com>
2024-04-11 03:03:16 +0300

    chore: constants: add CITY_STATS_LIFETIME_TICKS

--- constants.lua
+++ constants.lua
@@ -13,6 +13,7 @@ local CONSTANTS = {
     -- Each cell has 6x6 tiles
     CELL_SIZE = 6,
     CITY_GROWTH_TICKS = OneSecond * 60,
+    CITY_STATS_LIFETIME_TICKS = OneSecond * 30,
     CITY_RADIUS = 150,
     -- Minimum ticks until we try adding another city
     MORE_CITIES_TICKS = OneSecond * 120,
