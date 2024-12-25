3e6a8e2f432b6eec345e51945336c47bc19d3ee3
plexpt <plexpt@gmail.com>
2024-12-25 19:08:06 +0800

    update town-hall.png

--- prototypes/entities/town-center-virtual.lua
+++ prototypes/entities/town-center-virtual.lua
@@ -3,7 +3,7 @@ data:extend{
         type = "container",
         name = "tycoon-town-center-virtual",
         icon = "__tycoon__/graphics/entity/town-hall/town-hall.png",
-        icon_size = 64,
+        icon_size = 500,
         inventory_size = 0,
         collision_box = { { -5, -5}, {5, 5} },
         picture = {
@@ -11,9 +11,9 @@ data:extend{
                 {
                     filename = "__tycoon__/graphics/entity/town-hall/town-hall.png",
                     priority = "high",
-                    width = 250,
-                    height = 250,
-                    scale = 1,
+                    width = 500,
+                    height = 500,
+                    scale = 0.5,
                     shift = {0.6, 0}
                 },
             }
